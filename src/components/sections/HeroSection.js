b0b1598a49327c8372d901b5f0996d2e770eb4d6
Anh Nhut <anhnhut@MacBook-Pro.local>
2024-05-15 20:55:11 +0700

    Fix avatar responsive on  mobile

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -2,25 +2,25 @@ export default function HeroSection() {
   return (
     <section className="hero bg-gradient-to-b from-slate-900 from-5% via-slate-800 to-slate-900 to-90% text-gray-900 text-center">
       <div className="max-w-5xl mx-auto px-4 sm:px-8 pt-8 sm:pt-16 pb-12 sm:pb-24">
-        <div className="infomation group flex gap-1 items-center justify-center">
-          <div className="introdure text-slate-100 pl-8">
+        <div className="infomation group flex flex-col-reverse sm:flex-row gap-0 sm:gap-2 items-center justify-center">
+          <div className="introdure flex sm:block text-slate-100 pl-0 sm:pl-8">
             <h2 className="font-semibold">
-              <span className="block text-left text-slate-200 text-base sm:text-lg md:text-xl font-medium">
+              <span className="sm:block text-left text-slate-200 text-base sm:text-lg md:text-xl font-medium px-1">
                 Hi! I'm
               </span>
-              <span className="text-xl sm:text-2xl md:text-3xl text-amber-400 tracking-wide whitespace-nowrap">
+              <span className="text-xl sm:text-2xl md:text-3xl text-amber-400 tracking-wide whitespace-nowrap px-1">
                 Anh Nhut
               </span>
             </h2>
-            <p className="text-lg sm:text-xl md:text-2xl">(anhutf)</p>
+            <p className="text-lg sm:text-xl md:text-2xl px-1">(anhutf)</p>
           </div>
-          <div className="cover relative grow">
+          <div className="cover w-11/12 sm:w-full relative">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 39 24"
+              viewBox="0 0 39 22"
               stroke="currentColor"
-              className="w-full stroke-[1.5] sm:stroke-[1.2] transition duration-500 ease-in-out group-hover:scale-x-105 group-hover:text-slate-800 animate-pulse"
+              className="w-full stroke-[1.4] sm:stroke-[1.3] md:stroke-[1.2] transition duration-500 ease-in-out group-hover:scale-x-105 group-hover:text-slate-800 animate-pulse"
             >
               <path
                 strokeLinecap="round"
@@ -28,16 +28,16 @@ export default function HeroSection() {
                 d="M32.25 6.75 37.5 12l-5.25 5.25m-25.5 0L1.5 12l5.25-5.25m22.5-3-4.5 16.5"
               />
             </svg>
-            <div className="avatar absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-b-2 border-b-slate-800">
+            <div className="avatar w-1/2 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-b-2 border-b-slate-800">
               <img
-                className="w-auto h-full drop-shadow-2xl opacity-80 group-hover:opacity-90 z-20"
+                className="w-full drop-shadow-2xl opacity-80 group-hover:opacity-90 z-20"
                 src={require("../../assets/anhutf-sticker.png")}
               />
             </div>
           </div>
         </div>
 
-        <h1 className="text-amber-300 text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight z-50 drop-shadow-2xl">
+        <h1 className="text-amber-300 text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight z-50 drop-shadow-2xl pt-4">
           <span>Web Developer </span>
           <span className="text-amber-400"> & </span>
           <span className="whitespace-nowrap"> Front-End</span>
